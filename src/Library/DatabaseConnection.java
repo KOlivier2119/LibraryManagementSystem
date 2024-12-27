3bf3cbb9e02f2eb43335b7a54eabea3db41d58f9
KWIZERA Olivier <kwizeraolivier2119@gmail.com>
2024-12-27 19:34:50 +0200

    Changing password

--- src/Library/DatabaseConnection.java
+++ src/Library/DatabaseConnection.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/library_db";
     private static final String USER = "postgres";
-    private static final String PASSWORD = "Kom0852@";
+    private static final String password = "Kom0852@";
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+        return DriverManager.getConnection(URL, USER, password);
     }
 }
